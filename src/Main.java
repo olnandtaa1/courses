10d77ba9b48a9e9b819b3c10dfa7dc90b8a68ed8
olnandtaa1 <dasha.kravchenko.03@gmail.com>
2022-06-06 22:43:46 +0300

    Hello world/user + task 1 [updated]

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-
-        //System.out.print("Введите число a = ");
-
-
-        //System.out.print("Введите число b = ");
-
-        // int c = a + b;
-        //  int d = a - b;
+        {
+            System.out.println("Hello world");
+        }
+        System.out.print("Введите username: ");
+        Scanner sc = new Scanner(System.in);
+       String a1 = sc.nextLine();
+        System.out.println("Hello " + a1);
 
 
         int a, b;
         Scanner sc1 = new Scanner(System.in);
-        System.out.print("Введите число a = ");
-       // a = sc1.nextInt();
-        //Scanner sc2 = new Scanner(System.in);
-        System.out.print("Введите число b = ");
-        // b = sc1.nextInt();
-
-        if (sc1.hasNextInt()) {
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("Введите числа a и b: ");
+        if (sc1.hasNextInt() && (sc2.hasNextInt()))
+        {
             a = sc1.nextInt();
-            b = sc1.nextInt();
-
+            b = sc2.nextInt();
             int c = a + b;
             System.out.println("Значение выражения a + b = " + c);
             int d = a - b;
             System.out.println("Значение выражения a - b = " + d);
-
-        } else
+        }
+        else {
             System.out.print("Пожалуйста, повторите попытку");
+        }
     }
 }
 
